daa91fd85043a509fae3b6f171d51d3fc93a238b
collisj1996 <jackcollis96@gmail.com>
2022-06-12 17:04:55 +0100

    Add fib function

---
+++ fib.js
@@ -0,0 +1,17 @@
+// O(2^n) time
+// O(n) space
+const slowFib = (n) => {
+  if(n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+};
+
+// O(n) time
+// O(n) space
+const fastFib = (n, cache = {}) => {
+  if(n <= 2) return 1;
+  if(cache[n]) return cache[n];
+  cache[n] = fastFib(n - 1, cache) + fastFib(n - 2, cache);
+  return cache[n];
+};
+
+console.log(fastFib(50));
